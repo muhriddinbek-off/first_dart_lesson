2933daaf9711042c97d254a4743386f1ed2383f6
muhriddinbek-off <muhriddinusanov7@gmail.com>
2024-02-09 15:18:38 +0500

    first commet

---
+++ bin/first_dart_lesson.dart
@@ -0,0 +1,5 @@
+import 'package:first_dart_lesson/first_dart_lesson.dart' as first_dart_lesson;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_dart_lesson.calculate()}!');
+}
---
+++ test/first_dart_lesson_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_lesson/first_dart_lesson.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
